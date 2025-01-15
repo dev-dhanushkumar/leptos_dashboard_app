9e5908b5524191eeec6b7eecf78d56a00e851647
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-01-15 12:58:05 +0530

    Add core app components

---
+++ src/app/components.rs
@@ -0,0 +1,11 @@
+// For components
+pub mod header;
+pub use header::Header;
+
+pub mod add_persson_modal;
+pub use add_persson_modal::AddPersonModal;
+
+pub mod toast;
+pub use toast::Toast;
+pub use toast::ToastMessage;
+pub use toast::ToastMessageType;
