dc4f37efa1a31d036e1bb49926656620f4f15702
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-01-15 19:01:47 +0530

    Custom errors added

---
+++ src/app/errors/person_errors.rs
@@ -0,0 +1,29 @@
+// For person Errors
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PersonError {
+    #[error("members not found")]
+    PersonNotFound,
+    #[error("failed to update members")]
+    PersonUpdatefailure,
+    #[error("failed to create member")]
+    PersonCreateFailure,
+}
+
+pub type ErrorMessage = String;
+
+pub trait ResponseErrorTrait {
+    fn create(person_error: PersonError) -> ErrorMessage;
+}
+
+impl ResponseErrorTrait for ErrorMessage {
+    fn create(person_error: PersonError) -> ErrorMessage {
+        match person_error {
+            PersonError::PersonNotFound => String::from("member not found"),
+            PersonError::PersonUpdatefailure => String::from("failed to update member"),
+            PersonError::PersonCreateFailure => String::from("failed to create member"),
+        }
+    }
+}
