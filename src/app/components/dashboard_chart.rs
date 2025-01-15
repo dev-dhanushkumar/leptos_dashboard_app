76b80dbf4a3f718814ee6ba7c62df9c8f4ace5f7
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-01-15 16:55:27 +0530

    Add barchart in home page successfully!

--- src/app/components/dashboard_chart.rs
+++ src/app/components/dashboard_chart.rs
@@ -2,6 +2,7 @@ use crate::app::{components::DashboardWidget, models::Person};
 use leptos::*;
 use num_format::{Buffer, Locale};
 use std::rc::Rc;
+use charts_rs:: { BarChart, Series, Color, THEME_DARK};
 
 
 #[component]
@@ -21,6 +22,11 @@ pub fn DashboardChart(persons_data: Vec<Person>) -> impl IntoView {
     let mut latest_number: String = String::new();
     let mut counter: i32 = 0;
 
+    // 2 vectors for displaying the titles and the other for counting
+    // the quality/number for each title
+    let mut data_vec = Vec::new();
+    let mut count_vec = Vec::new();
+
     //loop through the returned data
     for person in persons_data {
         if counter == 0 {
@@ -30,8 +36,43 @@ pub fn DashboardChart(persons_data: Vec<Person>) -> impl IntoView {
         //adding this personcomponsation to the total team cost
         total_cost += person.compensation;
 
+        // if the vector for displaying the title doesn't continue this person's
+        //title yet
+        if !data_vec.contains(&person.title) {
+            // we add it to the vector
+            data_vec.push(person.title);
+
+            // we also add 1 to the count vector
+            count_vec.push(1.0);
+        } else {
+            //if this title has previously been added to the titles vector
+            // we get the index of the title in the titles vector
+            let index = data_vec
+                .iter()
+                .position(|title| title == &person.title)
+                .unwrap();
+
+            // we also get the number in the vector using that index
+            let num_at_index = count_vec[index];
+
+            // then we increment it by 1
+            count_vec[index] = num_at_index + 1.0;
+        }
+
         counter += 1;
     }
+
+    let mut bar_series = Series::new(String::new(), count_vec);
+    bar_series.label_show = true;
+
+    let mut bar_chart = BarChart::new_with_theme(vec![bar_series], data_vec, THEME_DARK);
+    bar_chart.font_family = String::from("Noto Sans SC");
+    bar_chart.background_color = Color::transparent();
+    bar_chart.width = 832.0;
+    bar_chart.height = 500.0;
+
+    // to not show the y-axis with the decimal point number for count
+    bar_chart.y_axis_hidden = true;
 
     // to convert the total cost to a string using the num-format crate's Buffer
     let mut buf = Buffer::default();
@@ -44,6 +85,12 @@ pub fn DashboardChart(persons_data: Vec<Person>) -> impl IntoView {
                 <DashboardWidget title="Team Members" value=&team_count />
                 <DashboardWidget title="Monthly Team Cost" value=&total_cost_str />
                 <DashboardWidget title="Just Joined" value=&latest_number />
+            </div>
+
+            <div class="max-w-[54rem] mx-auto w-full flex flex-col mt-14 pb-12">
+                <div class="w-full max-w-[41rem] h-20 bg-black-200 rounded py-10 px-4 pb-10"
+                    inner_html=&bar_chart.svg().unwrap()>
+                </div>
             </div>
         </div>
     }
