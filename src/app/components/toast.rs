c10b7f1e4a33bba08ac06099475acc0b13a5e8c5
dev-dhanushkumar <dhanush.m@kriyaetive.com>
2025-01-16 12:26:28 +0530

    toast style changes

--- src/app/components/toast.rs
+++ src/app/components/toast.rs
@@ -1,16 +1,16 @@
 use leptos::*;
 use std::time::Duration;
 
-const TOAST_PARENT_STYLE: &str = "flex flex-row top-0 h-16 w-full max-w-[61rem] max-auto
-    item-center justify-center align-center fixed -mt-36
+const TOAST_PARENT_STYLE: &str = "flex flex-row top-0 h-16 w-full max-w-[61rem]
+    mx-auto items-center justify-center align-center fixed -mt-36
     transition-all duration-1000 ease-in-out";
 
-const TOAST_PARENT_APPEAR_STYLE: &str = "flex flex-row top-0 h-16 w-full max-w-[61rem] max-auto
-    item-center justify-center align-center fixed mt-20
+const TOAST_PARENT_APPEAR_STYLE: &str = "flex flex-row top-0 h-16 w-full max-w-[61rem]
+    mx-auto items-center justify-center align-center fixed mt-20
     transition-all duration-1000 ease-in-out";
 
-const TOAST_STYLE: &str = "flex w-96 h-16 bg-[#333333] rounded px-10 py-4 text-white
-    -mt-36 transition-all duration-1000 ease-in-out item-center";
+const TOAST_STYLE: &str = "flex w-96 h-16 bg-[#333333] rounded px-10 py-4
+    text-white -mt-36 transition-all duration-1000 ease-in-out items-center";
 
 pub enum ToastMessageType {
     NewMemberAdded,
@@ -28,8 +28,8 @@ impl Toast for ToastMessage {
     fn create(toast_message_type: ToastMessageType) -> ToastMessage {
         match toast_message_type {
             ToastMessageType::NewMemberAdded => String::from("New member added"),
-            ToastMessageType::MemberUpdated => String::from("Member deleted"),
-            ToastMessageType::MemberDeleted => String::from("Member updated"),
+            ToastMessageType::MemberDeleted => String::from("Member deleted"),
+            ToastMessageType::MemberUpdated => String::from("Member updated"),
         }
     }
 }
